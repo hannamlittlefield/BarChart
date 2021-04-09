b4a0f67b17e7fd5357ceddfd7553497073f7bdac
Hanna Littlefield <hannamlittlefield@gmail.com>
2021-04-09 11:52:50 -0400

    working on svg

--- public/App.js
+++ public/App.js
@@ -2,15 +2,13 @@ var svgContainer = d3
     .select('#chart')
     .append('svg')
     .attr('width', 500)
-    .attr('height', 500)
-    .attr('border', 'black');
+    .attr('height', 500);
 
 d3.json(
     'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
-    .then(function(data){
-        svgContainer
-            .append('text')
-            .text('GDP')
+    .then(function(e, data){
+            // what to do with data here 
+
     }
     
     )
